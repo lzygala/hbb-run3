9d47f14cda713a24113764cf3496578dfa732af9
Lara Zygala <[lara@zygala.com]>
2026-08-19 10:11:13 -0500

    variable model name

--- fitting/combine_scripts/all_cards_Run3.sh
+++ fitting/combine_scripts/all_cards_Run3.sh
@@ -1,12 +1,14 @@
-combineCards.py \
-    y22=2022/datacards/srModel_2022/model_combined.txt \
-    y22EE=2022EE/datacards/srModel_2022EE/model_combined.txt \
-    y23=2023/datacards/srModel_2023/model_combined.txt \
-    y23BPix=2023BPix/datacards/srModel_2023BPix/model_combined.txt \
-    y24=2024/datacards/srModel_2024/model_combined.txt \
-    > full_Run3_srModel.txt
+MODEL_NAME="sr_mainModel"
 
-cp 202*/datacards/srModel_*/*_202*.root .
+combineCards.py \
+    y22=2022/datacards/${MODEL_NAME}_2022/model_combined.txt \
+    y22EE=2022EE/datacards/${MODEL_NAME}_2022EE/model_combined.txt \
+    y23=2023/datacards/${MODEL_NAME}_2023/model_combined.txt \
+    y23BPix=2023BPix/datacards/${MODEL_NAME}_2023BPix/model_combined.txt \
+    y24=2024/datacards/${MODEL_NAME}_2024/model_combined.txt \
+    > full_Run3_${MODEL_NAME}.txt
+
+cp 202*/datacards/${MODEL_NAME}_*/*_202*.root .
 
 
 # ONE POI : rH
@@ -19,7 +21,7 @@ cp 202*/datacards/srModel_*/*_202*.root .
 #     --PO 'map=.*/WHcc:rH[1,-20,20]' \
 #     --PO 'map=.*/ZHbb:rH[1,-20,20]' \
 #     --PO 'map=.*/ZHcc:rH[1,-20,20]'  \
-#     full_Run3_srModel.txt -o workspace_Run3.root
+#     full_Run3_${MODEL_NAME}.txt -o workspace_Run3.root
 
 # TWO POI : rH_bb , rH_cc
 # text2workspace.py -P HiggsAnalysis.CombinedLimit.PhysicsModel:multiSignalModel --PO verbose \
@@ -31,7 +33,7 @@ cp 202*/datacards/srModel_*/*_202*.root .
 #     --PO 'map=.*/WHcc:rH_cc[1,-20,20]' \
 #     --PO 'map=.*/ZHbb:rH_bb[1,-20,20]' \
 #     --PO 'map=.*/ZHcc:rH_cc[1,-20,20]'  \
-#     full_Run3_srModel.txt -o workspace_Run3.root
+#     full_Run3_${MODEL_NAME}.txt -o workspace_Run3.root
 
 # ONE bb POI // ALL cc POI : rH_bb // rVBF_cc , rggF_cc, rVH_cc
 # text2workspace.py -P HiggsAnalysis.CombinedLimit.PhysicsModel:multiSignalModel --PO verbose \
@@ -43,7 +45,7 @@ cp 202*/datacards/srModel_*/*_202*.root .
 #     --PO 'map=.*/WHcc:rVH_cc[1,-20,20]' \
 #     --PO 'map=.*/ZHbb:rH_bb[1,-20,20]' \
 #     --PO 'map=.*/ZHcc:rVH_cc[1,-20,20]'  \
-#     full_Run3_srModel.txt -o workspace_Run3.root
+#     full_Run3_${MODEL_NAME}.txt -o workspace_Run3.root
 
 # ALL POI : rVBF_bb , rggF_bb, rVH_bb // rVBF_cc , rggF_cc, rVH_cc
 text2workspace.py -P HiggsAnalysis.CombinedLimit.PhysicsModel:multiSignalModel --PO verbose \
@@ -55,4 +57,4 @@ text2workspace.py -P HiggsAnalysis.CombinedLimit.PhysicsModel:multiSignalModel -
     --PO 'map=.*/WHcc:rVH_cc[1,-20,20]' \
     --PO 'map=.*/ZHbb:rVH_bb[1,-20,20]' \
     --PO 'map=.*/ZHcc:rVH_cc[1,-20,20]'  \
-    full_Run3_srModel.txt -o workspace_Run3.root
+    full_Run3_${MODEL_NAME}.txt -o workspace_Run3.root
